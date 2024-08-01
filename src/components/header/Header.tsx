ac43adf76917b0c5991b5a9a2895120ae2fcd7ae
Dmitry Khodyrev <dooommm@mail.ru>
2024-08-01 12:51:31 +0800

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,5 @@
+import styles from './Header.module.scss';
+
+export const Header = () => {
+  return <div className={styles.headerWrapper}>Порядок организации выездных корпоративных мероприятий</div>;
+};
